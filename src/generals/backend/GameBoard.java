5ccabccda73f75f14fff7ce4caf02098a173564e
XKTZ <ericchenxktz@gmail.com>
2022-01-11 09:19:12 -0500

    Created animate panel

--- src/generals/backend/GameBoard.java
+++ src/generals/backend/GameBoard.java
@@ -1,10 +1,34 @@
 package generals.backend;
 
 /**
+ * The game board
+ *
  * @author Yidi Chen
  * @date 2022-01-11
  */
 public class GameBoard {
 
+    /**
+     * Game board
+     * use 9 * 10 instead of 8 * 9 to make the graph start with 1
+     */
+    private Chess[][] board = new Chess[9][10];
 
+    /**
+     * The turn on
+     */
+    private int intPlayerOn = 1;
+
+    /**
+     * The winner
+     */
+    private int intWinner;
+
+    public GameBoard() {
+
+    }
+
+    public Chess[][] getBoard() {
+        return board;
+    }
 }
---
+++ src/generals/ui/AnimatePanel.java
@@ -0,0 +1,12 @@
+package generals.ui;
+
+import javax.swing.*;
+
+/**
+ * Animating panel at the beginning of game
+ *
+ * @author Yidi Chen
+ * @date 2022-01-11
+ */
+public class AnimatePanel extends JPanel  {
+}
