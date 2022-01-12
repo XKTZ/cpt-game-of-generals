fb41e11db8e36fdd39333c865d8ea41f9d770654
Jornamu <90406479+Jornamu@users.noreply.github.com>
2022-01-12 09:21:21 -0500

    Added Moving words to Panel, to introduce game (in the Jframe and also tested)

--- src/generals/main/TestMain.java
+++ src/generals/main/TestMain.java
@@ -1,11 +1,20 @@
 package generals.main;
 
+import generals.ui.AnimatePanel;
+
+import javax.swing.*;
+
 /**
  * @author Yidi Chen
  * @date 2022-01-10
  */
 public class TestMain {
-    public static void main(String[] args) {
-        System.out.println("hello");
+    public static void main(String[] args){
+        JFrame jf = new JFrame();
+        jf.setSize(500,500);
+        jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        jf.add(new AnimatePanel());
+        jf.setLocationRelativeTo(null);
+        jf.setVisible(true);
     }
 }
--- src/generals/ui/AnimatePanel.java
+++ src/generals/ui/AnimatePanel.java
@@ -11,11 +11,38 @@ import java.awt.*;
  */
 public class AnimatePanel extends JPanel  {
 
+    int x=0;
+    int y=100;
+    int a =400;
+    int b =200;
     /**
      * Animate panel
      */
     public AnimatePanel() {
 
+    }
+
+    public void paint(Graphics gp) {
+        super.paint(gp);
+        Graphics2D g2d= (Graphics2D) gp;
+        g2d.drawString("Welcome to",x,y);
+        g2d.drawString("Games of Generals",a,b);
+        g2d.drawString("Enjoy",x,300);
+            try{
+                Thread.sleep(200);
+                x+=20;
+                a-=20;
+
+                if(x>getWidth()){
+                    x=0;
+                }
+                if(a<0){
+                    a=500;
+                }
+                repaint();
+            } catch (InterruptedException ex){
+                JOptionPane.showMessageDialog(this, ex);
+            }
     }
 
     public void start() {
