24c823778f6077ae765711c38dc561bd15e38c00
XKTZ <ericchenxktz@gmail.com>
2022-01-19 09:43:37 -0500

    Skeleton of GameImages

--- src/generals/frontend/ui/GameImages.java
+++ src/generals/frontend/ui/GameImages.java
@@ -1,18 +1,22 @@
 package generals.frontend.ui;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
-public class GameImages extends JPanel {
+public interface GameImages {
 
+    BufferedImage IMAGE_FLAG = imageOf("res/img/flag.png");
 
-
-    public GameImages() {
-        super();
-        try{
-            onesuncolonelimg = ImageIO.read(new File("1-sun-colonel.png"));
-        }catch(IOException e){
-            System.out.println("ERROR");
+    static BufferedImage imageOf(String path) {
+        try {
+            BufferedImage image = ImageIO.read(GameImages.class.getResourceAsStream(path));
+            return image;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
         }
     }
 }
