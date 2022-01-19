499ffbbb69ac63fd9f3a6c4a9fca6e17103afabb
Jornamu <90406479+Jornamu@users.noreply.github.com>
2022-01-19 09:22:10 -0500

    Started to build the Help panel, gonna start with loading images in the other panel cause thats a thing

--- src/generals/frontend/ui/HelpPanel.java
+++ src/generals/frontend/ui/HelpPanel.java
@@ -9,6 +9,12 @@ import javax.swing.*;
 public class HelpPanel extends JPanel {
 
     public HelpPanel() {
+        JFrame frame = new JFrame("HelpPanel");
+        frame.setVisible(true);
+        frame.setSize(900,720);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+        JLabel label = new Jlabel("Try to move the piece by dragging");
+        JPanel panel = new JPanel();
     }
 }
